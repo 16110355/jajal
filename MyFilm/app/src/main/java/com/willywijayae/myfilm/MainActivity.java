9a822ff5cbff4b8bbe479b6e0936eed3f7171784
16110355 <39642733+16110355@users.noreply.github.com>
2018-10-29 23:47:59 +0700

    tes

--- MyFilm/app/src/main/java/com/willywijayae/myfilm/MainActivity.java
+++ MyFilm/app/src/main/java/com/willywijayae/myfilm/MainActivity.java
@@ -1,29 +1,27 @@
 package com.willywijayae.myfilm;
 
-import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.bumptech.glide.Glide;
+import android.util.Log;
+import android.widget.ListView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-
-import okhttp3.Call;
-import okhttp3.Callback;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +29,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
-        Button buttonMove = findViewById(R.id.btnLihat);
+        /*Button buttonMove = findViewById(R.id.btn_lanjut);
         buttonMove.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,64 +37,76 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
 
             }
-        });
+        });*/
 
-        final TextView tvNama = findViewById(R.id.tvNama);
-        final TextView tvSinopsis = findViewById(R.id.tvSinopsis);
-        final ImageButton img = findViewById(R.id.imgButton);
 
-        String url = "https://api.themoviedb.org/3/movie/now_playing?" +
-                "api_key=32f7f07283696dba81c173ea5b35c87d";
+        listView = (ListView) findViewById(R.id.list);
 
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url(url)
-                .build();
-        client.newCall(request).enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
-                Toast.makeText(getApplicationContext(),
-                        "Tidak dapat terhubung server", Toast.LENGTH_LONG).show();
+        new CheckConnectionStatus().execute("https://api.themoviedb.org/3/movie/now_playing?api_key=32f7f07283696dba81c173ea5b35c87d");
+
+
+    }
+
+    class CheckConnectionStatus extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            URL url = null;
+            try{
+                url = new URL(params[0]);
+            } catch (MalformedURLException e) {
                 e.printStackTrace();
             }
+            try{
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
 
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                final String responseData = response.body().string();
-                try {
-                    JSONObject objData = new JSONObject(responseData);
-                    final JSONArray arrayNama = objData.getJSONArray("results");
-                    final JSONObject objNama = new JSONObject(arrayNama.get(0).toString());
+                InputStream inputStream = urlConnection.getInputStream();
 
-                    MainActivity.this.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            try {
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String s = bufferedReader.readLine();
+                bufferedReader.close();
 
-                                tvNama.setText(objNama.get("title").toString());
-                                tvSinopsis.setText(objNama.get("overview").toString());
-
-                                String urlIcon = "http://image.tmdb.org/t/p/w185"+
-                                        objNama.get("poster_path");
-                                Glide.with(MainActivity.this)
-                                        .load(urlIcon)
-                                        .into(img);
-
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-
-
-                        }
-                    });
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+                return s;
+            }catch (IOException e) {
+                Log.e("Error: ", e.getMessage(), e);
             }
-        });
+            return null;
+        }
 
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            JSONObject jsonObject = null;
 
+            try {
+                jsonObject = new JSONObject(s);
+
+                ArrayList<MovieDetails> movieList = new ArrayList<>();
+
+                JSONArray jsonArray = jsonObject.getJSONArray("results");
+
+                for (int i=0; i<jsonArray.length();i++)
+                {
+                    JSONObject object = jsonArray.getJSONObject(i);
+                    MovieDetails movieDetails = new MovieDetails();
+                    movieDetails.setOriginal_title(object.getString("original_title"));
+                    movieDetails.setVote_average(object.getString("vote_average"));
+                    movieDetails.setOverview(object.getString("overview"));
+                    movieDetails.setRelease_date(object.getString("release_date"));
+                    movieDetails.setPoster_path(object.getString("poster_path"));
+                    movieList.add(movieDetails);
+                }
+                MovieArrayAdapter movieArrayAdapter = new MovieArrayAdapter(MainActivity.this, R.layout.movie_list,movieList);
+                listView.setAdapter(movieArrayAdapter);
+            } catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
 
     }
 }
